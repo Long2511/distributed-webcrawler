4f15678324ba85c14bdef793d0dacba94c014489
xiinthanh <xiinthanh@gmail.com>
2025-06-30 10:38:06 +0700

    setup MongoDB using MongoConfig, replace 172.16.132.118 with master ip

--- src/main/java/com/ouroboros/webcrawler/config/MongoConfig.java
+++ src/main/java/com/ouroboros/webcrawler/config/MongoConfig.java
@@ -1,13 +1,37 @@
 package com.ouroboros.webcrawler.config;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+
+    @Value("${spring.data.mongodb.port}")
+    private int port;
+
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
+
     @Override
     protected String getDatabaseName() {
-        return "webcrawler";
+        return databaseName;
+    }
+
+    @Override
+    @Bean
+    public MongoClient mongoClient() {
+        String connectionString = String.format("mongodb://%s:%d/%s", host, port, databaseName);
+        return MongoClients.create(MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .build());
     }
 }
